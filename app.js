23ad4d6dc351c3bc5f205c293376bad51908e2cc
tskeens71 <tskeens5@ivytech.edu>
2024-09-22 17:55:57 -0400

    final

--- app.js
+++ app.js
@@ -4,34 +4,15 @@ const morgan = require('morgan');
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
-
 // register view engine
 app.set('view engine', 'ejs');
 
+// listen for requests
+app.listen(3000);
+
 // middleware & static files
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
-
 app.use(morgan('dev'));
-
-app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
-});
 
 app.get('/', (req, res) => {
   const blogs = [

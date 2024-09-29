2991bda341fe3e94c41020f300badd5c85b8b696
tskeens71 <tskeens5@ivytech.edu>
2024-09-29 02:55:51 -0400

    lesson-9

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const Blog = require('./models/blog');
 // express app
 const app = express();
 
-// connect to mongodb & listen for requests
+// connect to mongodb 
 const dbURI = "mongodb+srv://Tskeens71:<dante1971>@nodetuts.cy1t0.mongodb.net/?retryWrites=true&w=majority&appName=nodetuts";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -34,52 +34,4 @@ app.get('/add-blog', (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-});
-
-app.get('/all-blogs', (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get('/single-blog', (req, res) => {
-  Blog.findById('5ea99b49b8531f40c0fde689')
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
-});
-
-// blog routes
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-// 404 page
-app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
 });
